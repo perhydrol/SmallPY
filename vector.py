034be495c75868025c80fcfdea7986776a7b49a9
Hydrogen <h2o2ish2o2@outlook.com>
2022-04-23 14:34:10 +0800

    完成位置跟踪

--- vector.py
+++ vector.py
@@ -5,58 +5,93 @@ from matplotlib.pyplot import flag
 import numpy as np
 import cv2 as cv
 import mediapipe as mp
+from multiprocessing import Process, Pipe
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+mp_hand = mp.solutions.hands
 
 global wide
 global long
 
 capError = '错误：摄像头错误'
 
-def to_int(point):
-  point.x=point.x*wide
-  point.y=point.y*long
-  return point
-def to_int2(x,y):
-  return (int(x),int(y))
 
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print(capError+'Not Open.')
-    exit()
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)as pose:
-    flag_frist=False
-    last_right={}
+def to_int(point):
+    point.x = point.x*wide
+    point.y = point.y*long
+    return point
+
+
+def to_int2(x, y):
+    return (int(x), int(y))
+
+
+def Pose(p_vido: Pipe, p_res: Pipe):
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)as pose:
+        while (p_vido.poll(None)):
+            vido = p_vido.recv()
+            vido = cv.cvtColor(vido, cv.COLOR_BGR2RGB)
+            result = pose.process(vido)
+            if result.pose_landmarks:
+              landmarks = result.pose_landmarks.landmark
+              left_shoulder = landmarks[12]
+              right_shoulder = landmarks[11]
+              p_res.send([left_shoulder, right_shoulder])
+            else:
+              p_res.send([False])
+
+
+def Hand(p_vido: Pipe, p_res: Pipe):
+    with mp_hand.Hands(model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        while(p_vido.poll(None)):
+            vido = p_vido.recv()
+            vido = cv.cvtColor(vido, cv.COLOR_BGR2RGB)
+            result = hands.process(vido)
+            if result.multi_hand_landmarks:
+              landmarks = result.multi_hand_landmarks[0].landmark
+              res_hand = landmarks[0]
+              p_res.send([res_hand])
+            else:
+              p_res.send([False])
+
+
+if __name__ == '__main__':
+    p1_send_par, p1_send_chil = Pipe()
+    p1_rec_par, p1_rec_chil = Pipe()
+    p2_send_par, p2_send_chil = Pipe()
+    p2_rec_par, p2_rec_chil = Pipe()
+    Pose_process = Process(target=Pose, args=(p1_send_chil, p1_rec_chil,))
+    Hands_process = Process(target=Hand, args=(p2_send_chil, p2_rec_chil,))
+    Pose_process.start()
+    Hands_process.start()
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print(capError+'Not Open.')
+        exit()
     while True:
         success, vido = cap.read()
         if not success:
             print(capError+'Cannot Read')
             print('再次尝试中')
-            continue
-        wide = vido.shape[1]
-        long = vido.shape[0]
         vido.flags.writeable = False
-        vido = cv.cvtColor(vido, cv.COLOR_BGR2RGB)
-        result =pose.process(vido)
-        landmarks=result.pose_landmarks.landmark
-        left_shoulder=to_int(landmarks[12])
-        right_shoulder=to_int(landmarks[11])
-        right_hand=to_int(landmarks[19])
-        left_hand=to_int(landmarks[20])
-        if(not flag_frist):
-          last_right=copy(right_hand)
-          flag_frist=True
+        long = vido.shape[0]
+        wide = vido.shape[1]
+        p1_send_par.send(vido)
+        p2_send_par.send(vido)
+        shoulder = p1_rec_par.recv()
+        hand = p2_rec_par.recv()[0]
         vido.flags.writeable=True
-        cv.line(vido,to_int2(left_shoulder.x,left_shoulder.y),to_int2(right_shoulder.x,right_shoulder.y),(255,0,0),2)
-        cv.circle(vido,to_int2(right_hand.x,right_hand.y),3,(0,0,255),-1)
-        cv.circle(vido,to_int2(left_hand.x,left_hand.y),3,(0,0,255),-1)
-        vido=cv.cvtColor(vido, cv.COLOR_RGB2BGR)
-        #if(flag_frist and abs(right_hand.x-last_right.x)>abs(left_shoulder.x-right_shoulder)):
-        #  if(last_right.x):
-        #    pass
-        cv.imshow('vido', vido)
+        if shoulder[0]!=False:
+          to_int(shoulder[0]),to_int(shoulder[1])
+          cv.line(vido,to_int2(shoulder[0].x,shoulder[1].y),to_int2(shoulder[1].x,shoulder[1].y),(225,0,0),3)
+        if(not(hand==False)):
+          to_int(hand)
+          cv.circle(vido,to_int2(hand.x,hand.y),2,(225,0,0),-1)
+        cv.imshow('test', cv.flip(vido, 1))
         if cv.waitKey(1) == ord('q'):
-            break
-cap.release()
+          Pose_process.terminate()
+          Hands_process.terminate()
+          break
+    cap.release()
